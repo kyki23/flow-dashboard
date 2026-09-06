4adbd1595aa46fea40fae614859973357d8236da
Krisztián Kökössy <krisztian.kokossy@gmail.com>
2026-09-06 12:43:27 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+
+      <main>
+        <h1>Dashboard</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+function Sidebar() {
+    return (
+        <aside>
+            <h2>FLOW</h2>
+
+            <nav>
+                <p>Overview</p>
+                <p>Projects</p>
+                <p>Tasks</p>
+                <p>Calendar</p>
+                <p>Activity</p>
+                <p>Settings</p>
+            </nav>
+        </aside>
+    )
+}
+
+export default Sidebar
